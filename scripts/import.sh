81835581799dd1838dba4a5140a75edf023b5140
ThomasH <ThomasH@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2010-12-17 12:20:35 +0000

    3.1.125: TH Archive enhancment prototype  - first archive Prototype; it export automaticly and bz2 it  - the import (still) a simple shell script, there a lot of work to do  - all functions/procedures/scripts needs parametering

---
+++ scripts/import.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export=/var/www/logzilla/exports/$1.bz2
+if [ -s "$export" ]; then
+  echo "file found in the online store";
+else
+  echo "file not in the online store"
+
+#  echo "try to restore from backup"  
+#  echo "doing restore *brumm* *brumm*"
+#  if [ $? ]; then 
+#      echo "cant restore from backup"
+     exit  ;
+#  fi
+
+fi
+echo "Copying file to temp space"
+cp  -f $export /tmp/.
+cd /tmp
+echo "unzipping..."
+bunzip2 $1.bz2 
+mv -f $1 logs
+echo "loading into the database"
+mysqlimport -i -C -u root --password=mysql syslog /tmp/logs
+rm -f /tmp/logs
+
