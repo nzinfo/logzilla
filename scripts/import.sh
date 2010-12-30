ca8ecd37402a3a17ad7a004c567aea26c342cb36
ThomasH <ThomasH@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2010-12-30 11:09:20 +0000

    Simple import shell script

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export=/var/www/logzilla/exports/$1.bz2
+export=/var/www/logzilla/exports/$1
 if [ -s "$export" ]; then
   echo "file found in the online store";
 else
@@ -14,12 +14,12 @@ else
 
 fi
 echo "Copying file to temp space"
-cp  -f $export /tmp/.
+cp  -f $export /tmp/$1.bz2
 cd /tmp
 echo "unzipping..."
-bunzip2 $1.bz2 
+bunzip2 $1.bz2
 mv -f $1 logs
 echo "loading into the database"
 mysqlimport -i -C -u root --password=mysql syslog /tmp/logs
 rm -f /tmp/logs
-
+echo "all done"
