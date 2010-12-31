dc15d7d5262a06d7f92e7d95a8ad8fa4a25d5ffb
thomash <ThomasH@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2010-12-31 17:40:36 +0000

    - commented out the old archive stuff  - changed database location to localhost - we use socket then  - some parametering of the "hardcoded" /var/www/logzilla

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 mysql --user root --password=mysql -e "call export;" syslog
-for i in $( ls /var/www/logzilla/exports/*.txt );
+for i in $( ls /path_to_logzilla/exports/*.txt );
 do 
 	bzip2 $i
 done 
