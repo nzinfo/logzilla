cc270da45e3e6077da7a67f58f785d823beae039
ThomasH <ThomasH@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-01-27 11:48:57 +0000

    - 1st view on trap handling  - easy made trap import

---
+++ scripts/traps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+read host
+read ip
+
+# we only need the last oid/val pair
+while read oid val
+do
+	var1="$oid"
+	var2="$val"
+done
+
+# send it into the database with a traditional db_insert.pl
+echo -e "$host\t$1\t$var1\t$var2"|/path_to_logzilla/scripts/db_insert.pl
