584ae745dd34818efd93487252d2630d3438861c
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-02-19 07:18:01 +0000

    #31  - Initial look at snmp, noticed thatatraps.sh needed the new TS format included.  - Also note that traps.sh was not set to executable

--- scripts/install.pl
+++ scripts/install.pl
@@ -38,7 +38,7 @@ sub p {
 }
 
 my $version = "3.1";
-my $subversion = ".164";
+my $subversion = ".165";
 
 # Grab the base path
 my $lzbase = getcwd;
--- scripts/traps.sh
+++ scripts/traps.sh
@@ -10,5 +10,8 @@ do
 	var2="$val"
 done
 
+TS=`date +"%Y-%m-%d %H:%M:%S"`
 # send it into the database with a traditional db_insert.pl
-echo -e "$host\t$1\t$var1\tTRAP!: $var2"|/path_to_logzilla/scripts/db_insert.pl
+echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2"|/path_to_logzilla/scripts/db_insert.pl
+# - for debug (below)
+# echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2">/tmp/trap.log
