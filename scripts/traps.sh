4b35919f439675a429d3b45dc9dc6f58b883f398
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-02-20 04:12:20 +0000

    #31 Small chages to traps.sh so that it didn't truncate.

--- scripts/traps.sh
+++ scripts/traps.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
+#read host
+#read ip
+#
+## we only need the last oid/val pair
+#while read oid val
+#do
+	#var1="$oid"
+	#var2="$val"
+#done
+#
+#TS=`date +"%Y-%m-%d %H:%M:%S"`
+## send it into the database with a traditional db_insert.pl
+#echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2"|/path_to_logzilla/scripts/db_insert.pl
+## - for debug (below)
+## echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2">/tmp/trap.log
+
 read host
 read ip
+vars=
 
-# we only need the last oid/val pair
 while read oid val
 do
-	var1="$oid"
-	var2="$val"
+    if [ "$vars" = "" ]
+    then
+        vars="$oid = $val"
+    else
+        vars="$vars, $oid = $val"
+    fi
 done
 
 TS=`date +"%Y-%m-%d %H:%M:%S"`
-# send it into the database with a traditional db_insert.pl
-echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2"|/path_to_logzilla/scripts/db_insert.pl
-# - for debug (below)
-# echo -e "$TS\t$host\t$1\t$var1\tTRAP!: $var2">/tmp/trap.log
+echo -e "$TS\t$host\t$1\tSNMP\t$vars" |/path_to_logzilla/scripts/db_insert.pl
+echo -e "$TS\t$host\t$1\tSNMP\t$vars" >/tmp/trap.log
