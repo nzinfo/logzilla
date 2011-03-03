2ad4ac6a58cb5c5284305b7c8aa3cdf96512ea16
ThomasH <ThomasH@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-03-03 22:36:33 +0000

    - Archive Import  - Changes in the export procedure and archives table

--- scripts/sql/archives.sql
+++ scripts/sql/archives.sql
@@ -9,6 +9,7 @@ SET character_set_client = utf8;
 CREATE TABLE `archives` (
   `id` int(11)  unsigned NOT NULL AUTO_INCREMENT,
   `archive` varchar(32) NOT NULL,
+  `records` int(11),
   `note` varchar(30),
   PRIMARY KEY (`id`) )
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
