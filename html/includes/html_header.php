6cd2ab3191f8718c107ed7bf19b98f4412dc3ea3
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-05-05 19:39:25 +0000

    - Fix for 3.1->3.2 upgrades on ui_layout (missed email alerts table) and new help sql  - Update to add no-cache to html header

--- html/includes/html_header.php
+++ html/includes/html_header.php
@@ -16,11 +16,14 @@ getsettings();
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo $_SESSION['SITE_URL']?>favicon.ico" />
 <?php echo "<title>".$addTitle.": ".$_SESSION['PROGNAME']." ".$_SESSION['VERSION']."</title>\n"; ?>
 <meta name="Description" "<?php echo $_SESSION['PROGNAME']?>">
-<meta name="Keywords" 'LogZilla', 'Syslog', 'Syslog Tool', 'Syslog Analysis', 'Syslog Analyzer'>
+<meta name="Keywords" 'LogZilla', 'Syslog', 'Syslog Tool', 'Syslog Analysis', 'Syslog Analyzer', 'Syslog Management'>
 <meta name="Copyright" 'LogZilla, LLC'>
-<meta name="Author" 'Clayton Dukes - cdukes@logzilla.info'>
+<meta name="Author" 'Clayton Dukes - cdukes@logzilla.pro'>
 <meta http-equiv="Content-Language" content="EN">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<meta http-equiv="cache-control" content="no-cache,no-store,must-revalidate">
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="expires" content="0">
 
 <!-- BEGIN Import CSS -->
 <?php include ("css.php");?>
