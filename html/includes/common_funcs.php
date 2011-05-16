2f00aafe53c5fb250fb0b19784293a1092051b6c
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2011-05-16 14:06:44 +0000

    Bug when using intval with crc32

--- html/includes/common_funcs.php
+++ html/includes/common_funcs.php
@@ -831,34 +831,34 @@ function search($json_o, $spx_max=1000,$index="idx_logs idx_delta_logs",$spx_ip=
                 if (preg_match ('/,/', $val)) {
                     $pieces = explode(',',$val);
                     foreach ($pieces as $part) {
-                        $mnes[] .= intval(mne2crc($part));
+                        $mnes[] .= mne2crc($part);
                     }
                 } else {
-                    $mnes[] .= intval(mne2crc($val));
+                    $mnes[] .= mne2crc($val);
                 }
                 break;
             case 'sel_mne':
                 foreach ($val as $subkey=>$subval) {
                      // echo "SubKey = $subkey, SubVal = $subval\n";
-                    $mnes[] .= intval(mne2crc($subval));
+                    $mnes[] .= mne2crc($subval);
                 }
                 break;
             case 'eids':
                 if (preg_match ('/,/', $val)) {
                     $pieces = explode(',',$val);
                     foreach ($pieces as $part) {
-                        $eids[] .= intval($part);
+                        $eids[] .= $part;
                     }
                 } else {
 		    if ($val !== '') {
-                        $eids[] .= intval($val);
+                        $eids[] .= $val;
 		    }
                 }
                 break;
             case 'sel_eid':
                 foreach ($val as $subkey=>$subval) {
                      // echo "SubKey = $subkey, SubVal = $subval\n";
-                    $eids[] .= intval($subval);
+                    $eids[] .= $subval;
                 }
                 break;
             case 'programs':
