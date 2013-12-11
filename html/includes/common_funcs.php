e04c796877b2ef3268395a289e0dc56ebe29425b
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2013-12-11 06:28:43 +0000

    Replaced getRelativeTime function with a better one

--- html/includes/common_funcs.php
+++ html/includes/common_funcs.php
@@ -837,32 +837,57 @@ function is_type($var) {
     return $type;
 }
 
-// ----------------------------------------------------------------------
-// Returns relative time
-// Usage: getRelativeTime('2011-01-01 12:00:00'); 
-// ----------------------------------------------------------------------
 function plural($num) {
     if ($num != 1)
         return "s";
 }
 
-function getRelativeTime($date) {
-    $diff = time() - strtotime($date);
-    if ($diff<60)
-        return $diff . " second" . plural($diff) . " ago";
-    $diff = round($diff/60);
-    if ($diff<60)
-        return $diff . " minute" . plural($diff) . " ago";
-    $diff = round($diff/60);
-    if ($diff<24)
-        return $diff . " hour" . plural($diff) . " ago";
-    $diff = round($diff/24);
-    if ($diff<7)
-        return $diff . " day" . plural($diff) . " ago";
-    $diff = round($diff/7);
-    if ($diff<4)
-        return $diff . " week" . plural($diff) . " ago";
-    return "on " . date("F j, Y", strtotime($date));
+// ----------------------------------------------------------------------
+// Returns relative time
+// Usage: getRelativeTime('2011-01-01 12:00:00'); 
+// ----------------------------------------------------------------------
+function getRelativeTime($ts) {
+    if(!ctype_digit($ts))
+        $ts = strtotime($ts);
+
+    $diff = time() - $ts;
+    if($diff == 0)
+        return 'now';
+    elseif($diff > 0)
+    {
+        $day_diff = floor($diff / 86400);
+        if($day_diff == 0)
+        {
+            if($diff < 60) return "$diff seconds ago";
+            if($diff < 120) return '1 minute ago';
+            if($diff < 3600) return floor($diff / 60) . ' minutes ago';
+            if($diff < 7200) return '1 hour ago';
+            if($diff < 86400) return floor($diff / 3600) . ' hours ago';
+        }
+        if($day_diff == 1) return 'Yesterday';
+        if($day_diff < 7) return $day_diff . ' days ago';
+        if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
+        if($day_diff < 60) return 'last month';
+        return date('F Y', $ts);
+    }
+    else
+    {
+        $diff = abs($diff);
+        $day_diff = floor($diff / 86400);
+        if($day_diff == 0)
+        {
+            if($diff < 120) return 'in a minute';
+            if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
+            if($diff < 7200) return 'in an hour';
+            if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
+        }
+        if($day_diff == 1) return 'Tomorrow';
+        if($day_diff < 4) return date('l', $ts);
+        if($day_diff < 7 + (7 - date('w'))) return 'next week';
+        if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
+        if(date('n', $ts) == date('n') + 1) return 'next month';
+        return date('F Y', $ts);
+    }
 }
 
 function array_recurse(&$array) {
