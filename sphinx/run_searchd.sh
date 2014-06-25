9f7d8a002cf67cd8794ed2725f691c2fa277230d
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2014-06-25 16:53:20 +0000

    Added a search audit table for autogenerating stopwords

--- sphinx/run_searchd.sh
+++ sphinx/run_searchd.sh
@@ -7,13 +7,13 @@ allargs=$@
 if [[ $@ == **stop ]]; then
     if [[ `pgrep -f "bin/searchd"` ]]; then
         echo "Stopping Searchd"
-        /var/www/logzilla/sphinx/bin/searchd -c /var/www/logzilla/sphinx/sphinx.conf --stop && echo $?
+        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stop && echo $?
     fi
 fi
 if [[ $@ == **stopwait ]]; then
     if [[ `pgrep -f "bin/searchd"` ]]; then
         echo "Stopping Searchd"
-        /var/www/logzilla/sphinx/bin/searchd -c /var/www/logzilla/sphinx/sphinx.conf --stopwait && echo $?
+        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait && echo $?
     fi
 fi
 
@@ -25,23 +25,23 @@ if [[ `pgrep -f "bin/searchd"` ]]; then
     echo "Searchd is already running"
     exit 1
 fi
-idxARR+=(`/var/www/logzilla/sphinx/bin/searchd -c /var/www/logzilla/sphinx/sphinx.conf | grep "sph: No such file or" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
+idxARR+=(`/path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf | grep "sph: No such file or" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
 uniq=$(echo "${idxARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 if [ ${#idxARR[@]} -gt 0 ]; then
     for index in "${idxARR[@]}"
     do
         echo "Creating Missing Index: $index"
-        errARR+=(`/var/www/logzilla/sphinx/bin/indexer -c /var/www/logzilla/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
+        errARR+=(`/path_to_logzilla/sphinx/bin/indexer -c /path_to_logzilla/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
     done
     uniq=$(echo "${errARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
     for index in "${uniq[@]}"
     do
         date=`echo $index | awk -F '_' '{print $5}' | sed 's/^\(.\{4\}\)\(.\{2\}\)/\1-\2-/' `
         echo "Recreating Missing SQL Views for $date"
-        /var/www/logzilla/scripts/LZTool -v -r makeview -mvdate $date -y 
+        /path_to_logzilla/scripts/LZTool -v -r makeview -mvdate $date -y 
     done
 fi
 if [[ `pgrep -f "bin/searchd"` ]]; then
-        /var/www/logzilla/sphinx/bin/searchd -c /var/www/logzilla/sphinx/sphinx.conf --stopwait 
-        /var/www/logzilla/sphinx/bin/searchd -c /var/www/logzilla/sphinx/sphinx.conf $allargs && echo $?
+        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait 
+        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf $allargs && echo $?
 fi
