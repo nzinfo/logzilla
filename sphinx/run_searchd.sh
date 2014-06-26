de1b530b82087aedd49685e508350d8b8cb2c51d
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2014-06-26 17:07:13 +0000

    Fix paths

--- sphinx/run_searchd.sh
+++ sphinx/run_searchd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+lzhome="/path_to_logzilla"
+[ ! -d "$lzhome" ] && lzhome="/var/www/logzilla"
 ulimit -Hn 25000
 ulimit -Sn 25000
 allargs=$@
@@ -7,14 +9,14 @@ allargs=$@
 if [[ $@ == **stop ]]; then
     if [[ `pgrep -f "bin/searchd"` ]]; then
         echo "Stopping Searchd"
-        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stop && echo $?
+        $lzhome/sphinx/bin/searchd -c $lzhome/sphinx/sphinx.conf --stop && echo $?
         exit
     fi
 fi
 if [[ $@ == **stopwait ]]; then
     if [[ `pgrep -f "bin/searchd"` ]]; then
         echo "Stopping Searchd"
-        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait && echo $?
+        $lzhome/sphinx/bin/searchd -c $lzhome/sphinx/sphinx.conf --stopwait && echo $?
         exit
     fi
 fi
@@ -25,14 +27,14 @@ fi
 # Make sure we're stopped first
 if [[ `pgrep -f "bin/searchd"` ]]; then
     echo "Stopping Search Daemon"
-    /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait && echo $?
+    $lzhome/sphinx/bin/searchd -c $lzhome/sphinx/sphinx.conf --stopwait && echo $?
 fi
-idxARR+=(`/path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf | grep "sph: No such file or" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
+idxARR+=(`$lzhome/sphinx/bin/searchd -c $lzhome/sphinx/sphinx.conf | grep "sph: No such file or" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
 uniq=$(echo "${idxARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 for index in "${idxARR[@]}"
 do
     echo "Creating Missing Index: '$index'"
-    errARR+=(`/path_to_logzilla/sphinx/bin/indexer -c /path_to_logzilla/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
+    errARR+=(`$lzhome/sphinx/bin/indexer -c $lzhome/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
 done
 uniq=$(echo "${errARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 for index in "${uniq[@]}"
@@ -40,7 +42,7 @@ do
     date=`echo $index | awk -F '_' '{print $5}' | sed 's/^\(.\{4\}\)\(.\{2\}\)/\1-\2-/' `
     if [[ "$date" ]]; then
         echo "Recreating Missing SQL Views for $date"
-        /path_to_logzilla/scripts/LZTool -v -r makeview -mvdate $date -y 
+        $lzhome/scripts/LZTool -v -r makeview -mvdate $date -y 
     fi
 done
 if [[ `pidof searchd` ]]; then
