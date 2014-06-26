953819c818595c634825ed2078a34d3044e724a7
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2014-06-26 17:03:35 +0000

    Fix check for missing index dates

--- sphinx/run_searchd.sh
+++ sphinx/run_searchd.sh
@@ -25,27 +25,25 @@ fi
 # Make sure we're stopped first
 if [[ `pgrep -f "bin/searchd"` ]]; then
     echo "Stopping Search Daemon"
-        /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait && echo $?
+    /path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait && echo $?
 fi
 idxARR+=(`/path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf | grep "sph: No such file or" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
 uniq=$(echo "${idxARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-if [ ${#idxARR[@]} -gt 0 ]; then
-    for index in "${idxARR[@]}"
-    do
-        echo "Creating Missing Index: $index"
-        errARR+=(`/path_to_logzilla/sphinx/bin/indexer -c /path_to_logzilla/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
-    done
-    uniq=$(echo "${errARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-    for index in "${uniq[@]}"
-    do
-        date=`echo $index | awk -F '_' '{print $5}' | sed 's/^\(.\{4\}\)\(.\{2\}\)/\1-\2-/' `
+for index in "${idxARR[@]}"
+do
+    echo "Creating Missing Index: '$index'"
+    errARR+=(`/path_to_logzilla/sphinx/bin/indexer -c /path_to_logzilla/sphinx/sphinx.conf $index | grep "range-query fetch failed" | awk '{print $3}' | sed "s/'//g" | sed 's/://g'`)
+done
+uniq=$(echo "${errARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+for index in "${uniq[@]}"
+do
+    date=`echo $index | awk -F '_' '{print $5}' | sed 's/^\(.\{4\}\)\(.\{2\}\)/\1-\2-/' `
+    if [[ "$date" ]]; then
         echo "Recreating Missing SQL Views for $date"
         /path_to_logzilla/scripts/LZTool -v -r makeview -mvdate $date -y 
-    done
-fi
+    fi
+done
 if [[ `pidof searchd` ]]; then
     echo "Starting Search Daemon"
     kill -HUP `pidof searchd`
-        #/path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf --stopwait 
-        #/path_to_logzilla/sphinx/bin/searchd -c /path_to_logzilla/sphinx/sphinx.conf $allargs && echo $?
 fi
