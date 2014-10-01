ed704e3a4196e7b16fa4fdb3fc891be857ed515e
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2014-10-01 05:39:19 +0000

    Updates for VM install

--- scripts/VM/bootup.sh
+++ scripts/VM/bootup.sh
@@ -2,13 +2,32 @@
 # Simple script used to wait for MySQL prior to starting Sphinx
 # Also sets console message for VMs
 # This script is called from /etc/rc.local during bootup
+
+lzhome="/path_to_logzilla"
+[ ! -d "$lzhome" ] && lzhome="/path_to_logzilla"
+
 for i in 1 2 3 4 5 6; do
-	if [ -S /var/run/mysql/mysql.sock ]; then
-	break
-	else
-		sleep 1
-		echo -n "."
-	fi
+    if [ -S /var/run/mysql/mysql.sock ]; then
+        break
+    else
+        sleep 1
+        echo -n "."
+    fi
 done
-(cd /path_to_logzilla/sphinx && bin/searchd)
-(cd /path_to_logzilla/scripts/VM && ./banner.pl)
+if [ -f $lzhome/scripts/VM/firstboot ]; then
+    wget --timeout=2 -q -O /tmp/test "http://www.logzilla.net/pingme" || rm /tmp/test
+    if [ -f /tmp/test ]; then
+        $lzhome/scripts/VM/update.pl
+        rm -f $lzhome/scripts/VM/firstboot
+    else
+        printf "\n\033[1m\tERROR!\n\033[0m\n"
+        echo "LogZilla requires internet access upon first boot. Please configure your network properly then reboot the system"
+        echo "LogZilla requires internet access upon first boot. Please configure your network properly then reboot the system" > /etc/issue
+        echo "LogZilla requires internet access upon first boot. Please configure your network properly then reboot the system" > /etc/issue.net
+        restart tty1
+        exit 1
+    fi
+fi
+(cd /path_to_logzilla/sphinx && ./run_searchd.sh --stop)
+(cd /path_to_logzilla/sphinx && ./run_searchd.sh)
+(cd $lzhome/scripts/VM && ./banner.pl)
