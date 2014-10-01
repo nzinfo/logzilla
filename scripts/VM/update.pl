9aa64aabe7e4b706673d79a2b56050202f649dac
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2014-10-01 03:52:22 +0000

    VM Update script

---
+++ scripts/VM/update.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+use Digest::MD5 qw(md5_hex);
+use File::Slurp qw(read_file write_file);
+
+# Change the skip license option to 'y'
+
+my $filename = '/var/www/logzilla/scripts/.lzrc';
+my $data = read_file $filename, {binmode => ':utf8'};
+$data =~ s/skiplic = "N"/skiplic = "Y"/g;
+write_file $filename, {binmode => ':utf8'}, $data;
+
+# Run an upgrade
+
+my $osupdate = system("apt-get update && apt-get -y upgrade")
+my $svnup = system("cd /var/www/logzilla/ && svn update --accept theirs-conflict");
+my $upgrade = system("cd /var/www/logzilla/scripts && ./upgrade");
+
+# Remove the test message and host
+my $remhost = system("/var/www/logzilla/scripts/LZTool -delhost -host host-1");
+
+
+# Change the skip license option back to 'n'
+my $filename = '/var/www/logzilla/scripts/.lzrc';
+my $data = read_file $filename, {binmode => ':utf8'};
+$data =~ s/skiplic = "Y"/skiplic = "N"/g;
+write_file $filename, {binmode => ':utf8'}, $data;
+
+# Remove the script from init.d and delete it
+
+my $remjob = ("update-rc.d update.pl remove");
+my $remscript = ("rm /etc/init.d/update.pl");
