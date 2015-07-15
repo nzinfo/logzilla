63f5320eb0d8d35636f2c525051d4febb3e02ba7
cdukes <cdukes@bd26d26f-a3c2-4b97-af7b-2ce88aa340fb>
2015-07-15 22:04:24 +0000

    Found and squashed old rbac bug

--- scripts/sql/rbac.sql
+++ scripts/sql/rbac.sql
@@ -50,25 +50,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
---
--- Dumping routines for database 'honzik'
---
-/*!50003 DROP FUNCTION IF EXISTS `rbac` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = '' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 FUNCTION `rbac`(have decimal(11,0), should decimal(11,0)) RETURNS tinyint(1) READS SQL DATA DETERMINISTIC return ((have&should)/should)=1 */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-
--- Dump completed on 2011-10-14 17:51:13
